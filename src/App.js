83502aad213f12ca9c5c9a7605f5507fa8bf45bf
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-29 19:48:43 +0530

    Add provider to main file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Provider, provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Redux Project</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>My Redux Project</h1>
+      </div>
+      </Provider>
   );
 }
 
