bd123b26abc88acba55b065916bae6cabc30bb35
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-30 00:09:48 +0530

    Add album list

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -3,14 +3,30 @@ import { use } from "react";
 function AlbumsList() {
 
     const dispatch = useDispatch();
+    const album = useSelector((state) => state.album.album);
+
+    const { loading, albums, error } = useSelector((state) => state.album);
 
     useEffect(() => {
         dispatch(fetchAlbums());
-    }, []);
+    }, [dispatch]);
+
+    console.log(album)
 
     return (
         <div>
             <h1>My Album List</h1>
+            <div>
+                {albums.map(album => (
+                    <div key={album.id}>
+                        {
+                            album.title
+                        }
+                    </div>
+
+                ))}
+
+            </div>
         </div>
     );
 }
