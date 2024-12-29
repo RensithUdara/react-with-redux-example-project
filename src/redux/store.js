985bd8c56bfbc9ec8b4ce06397db747a8690055a
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-29 23:10:07 +0530

    import packages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore , combineReducers} from "redux";
 import fruitReducer from "./fruitSection/fruitReducer";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 const rootReducer = combineReducers({
     fruit: fruitReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk , logger));
 
 export default store;
